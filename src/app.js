7117b3d101db44d1beaf50630b9525211bce0819
sauravphulera <phulerasanju20@gmail.com>
2025-04-07 10:28:38 +0530

    encryption

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const connectDB = require("./config/database");
 const User = require("./models/user");
 const validateUserData = require("./utils");
 const app = express();
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 app.use(express.json());
 
@@ -26,11 +28,39 @@ app.get("/devTinder/user", async (req, res) => {
 app.post("/devTinder/signup", async (req, res) => {
   try {
     validateUserData(req);
+    const { password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
     // create new user model instance
-    const user = new User(req.body);
+    const user = new User({ ...req.body, password: passwordHash });
 
     await user.save();
     res.send({ msg: "successfully saved user" });
+  } catch (e) {
+    res.status(500).send({ msg: e.message });
+  }
+});
+
+// login
+app.post("/devTinder/login", async (req, res) => {
+  try {
+    const { password, email } = req.body;
+    if (!validator.isEmail(email)) {
+      throw new Error("Invalid Email id");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new Error("Invalid Credentials");
+    }
+    const isPasswordValid = await bcrypt.compare(password, user?.password);
+    if (isPasswordValid) {
+      res.cookie("token", "adnsadjksandkjakdn");
+      res.send({ msg: "login successful" });
+    } else {
+      res.status(400).send({ msg: "Invalid Credentials" });
+    }
   } catch (e) {
     res.status(500).send({ msg: e.message });
   }
